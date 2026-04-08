321fd008a36f117277f6b154574caf885f517d3d
Alexey Nikitin <alexey.nikitin6723@gmail.com>
2026-04-08 14:17:17 +0400

    Init commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,26 @@
+import AddTaskForm from './AddTaskForm';
+import Button from './Button';
+import SearchTaskForm from './SearchTaskForm';
+import TodoInfo from './TodoInfo';
+import TodoList from './TodoList';
+import { useContext } from 'react';
+import { TasksContext } from '../context/TasksContext';
+
+
+const Todo = () => {
+  const { firstIncompleteTaskRef } = useContext(TasksContext)
+  return (
+    <div className="todo">
+      <h1 className="todo__title">To Do List</h1>
+      <AddTaskForm />
+      <SearchTaskForm />
+      <TodoInfo />
+      <Button 
+        onClick={() => firstIncompleteTaskRef.current?.scrollIntoView({ behavior: 'smooth' })}
+      >Show first incomplete task</Button>
+      <TodoList />
+    </div>
+  )
+}
+
+export default Todo;
