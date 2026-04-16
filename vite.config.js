11d828f5416202e1d0b33432d9612b33623bb99c
Alexey Nikitin <alexey.nikitin6723@gmail.com>
2026-04-16 16:19:24 +0400

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const isProd = mode === 'production'
 
   return {
-    base: isProd ? '/ToDoList' : '/',
+    base: isProd ? '/ToDoList/' : '/',
     plugins: [react()],
     resolve: {
       alias: {
